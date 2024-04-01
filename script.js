e9d33832214a09048ae056828f6a83768d25f173
zencmd <160051926+zencmd@users.noreply.github.com>
2024-04-01 16:41:24 +0000

    e

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const muteButton = document.getElementById("muteButton");
 var unmuteIcon = '<i class="fas fa-volume-up"></i>'
 var muteIcon = '<i class="fas fa-volume-mute"></i>'
 function toggleMute() {
-  if (audio.muted) {
-    audio.muted = false;
+  if (audio.paused) {
+    audio.play();
     muteButton.innerHTML = `<i class="fas fa-volume-up"></i>`;
   } else {
-    audio.muted  = true;
+    audio.pause();
     muteButton.innerHTML = `<i class="fas fa-volume-off"></i>`;
   }
 }
 
+audio.addEventListener('ended', function() {
+  audio.currentTime = 0;
+  audio.play();
+});
 
+audio.muted = true;
+audio.play()
+audio.muted = false;
 
 muteButton.addEventListener("click", toggleMute);
-
-
-function play() {
-  audio.play();
-}
-
-body.addEventListener('mousemove', play);
