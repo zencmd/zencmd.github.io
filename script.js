5c7a4305426086501ea2e217e245a30edac99b83
zencmd <160051926+zencmd@users.noreply.github.com>
2024-04-03 16:15:28 +0000

    e

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const pages = document.getElementById("pagesButton");
 var unmuteIcon = '<i class="fas fa-volume-up"></i>'
 var muteIcon = '<i class="fas fa-volume-mute"></i>'
 function toggleMute() {
-  if (audio.paused) {
-    audio.play();
-    muteButton.innerHTML = `<i class="fas fa-volume-up"></i>`;
-  } else {
-    audio.pause();
-    muteButton.innerHTML = `<i class="fas fa-volume-off"></i>`;
+  if (audio.playing = true) {
+    if (audio.muted) {
+      audio.muted = false;
+      muteButton.innerHTML = `<i class="fas fa-volume-up"></i>`;
+    } else {
+      audio.muted = true;
+      muteButton.innerHTML = `<i class="fas fa-volume-off"></i>`;
+    }
   }
 }
 
